c0462efa82d5f242f1ec2b0c6f4cd3616ec1641a
Valter Fernandes <fpvalter@gmail.com>
2019-11-04 16:54:05 -0300

    Route Class

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,57 @@
 <?php
 
+Route::get('login', function () {
+    return view('login');    
+})->name('login');
+
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::middleware([])->group(function () {
+
+    Route::prefix('admin')->group(function () {
+        
+        Route::namespace('Admin')->group(function () {
+        
+            Route::get('/dashboard', 'TesteController@teste')->name('admin.dashboard');
+        
+            Route::get('/financeiro', 'TesteController@teste')->name('admin.financeiro');
+            
+            Route::get('/produtos', 'TesteController@teste')->name('admin.produtos');
+
+            Route::get('/', 'TesteController@teste')->name('admin.home');
+
+        });
+        
+    });
+    
+}); */
+
+Route::group([
+    'middleware' => [],
+    'prefix' => 'admin',
+    'namespace' => 'Admin'
+], function () {
+    
+        Route::get('/dashboard', 'TesteController@teste')->name('admin.dashboard');
+        
+        Route::get('/financeiro', 'TesteController@teste')->name('admin.financeiro');
+        
+        Route::get('/produtos', 'TesteController@teste')->name('admin.produtos');
+
+        Route::get('/', 'TesteController@teste')->name('admin.home');
+    
+
+});
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('categorias/{flag}', function ($flag) {
+    return "Produtos da categoria {$flag}";
+});
+
+Route::get('produtos/{idProduct?}', function ($idProduct = null) {
+    return "Produtos {$idProduct}";
 });
